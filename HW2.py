83ef8ec4b3afd3546dd702210d67bfd1f54810a0
Ohad <2000ohad@gmail.com>
2026-03-24 20:14:18 +0200

    comitt

--- HW2.py
+++ HW2.py
@@ -5,16 +5,16 @@ while True:
     _id.append(input("enter ID of voter "))
     voting_tries += 1
     if _id[-1] == '999':
+        voting_tries -= 1
         _id.remove('999')
         break
 voters = set(_id)
 print(voters)
-invalid_voters = []
+invalid_voters = set()
 for x in _id:
     if _id.count(x) > 1:
-        invalid_voters.append(x)
+        invalid_voters.add(x)
 
 print(invalid_voters)
-invalid_voters_set = set(invalid_voters)
-voters -= invalid_voters_set
-print(f"the valid voters are: {voters}, the invalid ones are: {invalid_voters_set}, and people tried to vote {voting_tries} times")
+voters -= invalid_voters
+print(f"the valid voters are: {voters}, the invalid ones are: {invalid_voters}, and people tried to vote {voting_tries} times")
